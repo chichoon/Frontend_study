1f0c5245b9df02a9cc8aa395c932f0bae04cd1d3
Ji Yoon Choi <chichoon.choi@gmail.com>
2021-02-20 11:49:16 +0900

    update bg.js

--- JS/0220/bg.js
+++ JS/0220/bg.js
@@ -2,6 +2,17 @@ const body = document.querySelector("body");
 
 const IMG_NUM = 15;
 
+function handleImageLoad(){
+    body.appendChild(image);
+}
+
+function paintImage(imgNum){
+    //난수에 대응하는 사진을 배경으로 지정하는 함수
+    const image = new Image();
+    image.src = `images/${imgNum}.jpg`;
+    image.addEventListener("loadend", handleImageLoad);
+}
+
 function genRandom(){ //랜덤 숫자를 생성하는 함수
     const number = Math.floor(Math.random() * IMG_NUM);
     //math.floor 통해서 소수점을 지워버림
@@ -10,6 +21,7 @@ function genRandom(){ //랜덤 숫자를 생성하는 함수
 
 function init(){
     const randnum = genRandom();
-
+    paintImage(randnum);
+    //랜덤하게 이미지를 배경으로 지정
 }
 init();
