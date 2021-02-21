cfb4ffd336e67fdc9ea075f57a7de4ae3b80de8c
Ji Yoon Choi <chichoon.choi@gmail.com>
2021-02-21 22:34:18 +0900

    update readme.md & weather.js

--- JS/0220/README.md
+++ JS/0220/README.md
@@ -6,3 +6,9 @@
 - **Math.ceil()** : 올림 (3.1, 3.5, 3.6 -> 4)
 
 ## Getting the Weather part One
+- **navigator** object를 이용하면 다양한 api를 이용할 수 있음
+- **navigator.geolocation**은 장소 (좌표) 와 관련된 object
+    - **getCurrentPosition(함수1, 함수2)** 함수를 이용하여 현재 위치의 좌표를 불러올 수 있다
+    - 함수1은 좌표 불러오기에 성공했을 경우 실행됨
+    - 함수2는 좌표 불러오기에 실패했을 경우 실행됨
+    - ![좌표 불러오기 성공](./JSgeolocationSuccess.png)
--- JS/0220/weather.js
+++ JS/0220/weather.js
@@ -1,12 +1,22 @@
 const COORDS = 'coords';
 
+function handleGeoSuccess(position){ //좌표를 가져오는데 성공했을 경우 실행되는 함수
+    console.log(position);
+}
+
+function handleGeoError(){
+    console.log('failed to get position');
+}
+
 function askForCoords(){ //api를 이용해서 현재 위치를 받아옴
-    navigator.geolocation.getCurrentPosition()
+    navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+    //함수 두 개를 인자로 받는데, 첫 번째는 좌표 불러오기에 성공했을 경우,
+    //두 번째는 좌표 불러오기에 실패했을 경우
 }
 
 function loadCoords(){ //현재 위치 좌표를 불러오는 함수
     const loadedCoords = localStorage.getItem(COORDS);
-    if(loadCoords === null){
+    if(loadedCoords === null){
         askForCoords();
         //현재 위치 좌표가 저장되어 있지 않다면 새로 불러오기
     } else {
